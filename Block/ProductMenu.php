2eaf46e6503ce5298040da62daf568ea99865f97
nordraw <nikita.tretyak093@gmail.com>
2022-01-13 11:36:31 +0200

    Initial commit

---
+++ Block/ProductMenu.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tretiak\ProductMenu\Block;
+
+class ProductMenu extends \Magento\Framework\View\Element\Template
+{
+    protected $productCollectionFactory;
+    protected $categoryFactory;
+    protected $session;
+    protected $currentCategoryId;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param \Magento\Catalog\Model\CategoryFactory $categoryFactory
+     * @param \Magento\Catalog\Model\Session $session
+     * @param array $data
+     */
+    public function __construct(\Magento\Framework\View\Element\Template\Context               $context,
+                                \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+                                \Magento\Catalog\Model\CategoryFactory                         $categoryFactory,
+                                \Magento\Catalog\Model\Session                                 $session,
+                                array                                                          $data = [])
+    {
+        $this->productCollectionFactory = $productCollectionFactory;
+        $this->categoryFactory = $categoryFactory;
+        $this->session = $session;
+
+        $this->getCurrentCategoryId();
+
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Get 3 simple products per page
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
+     */
+    public function getSimpleProducts()
+    {
+        $collection = $this->productCollectionFactory->create();
+
+        $collection->addAttributeToSelect('*');
+
+        $collection->addAttributeToFilter('size', ['null' => true]);
+        $collection->addAttributeToFilter('color', ['null' => true]);
+
+        $category = $this->categoryFactory->create()->load($this->currentCategoryId);
+        $collection->addCategoryFilter($category);
+
+        $collection->getSelect()->orderRand();
+        $collection->getSelect()->limit(3);
+
+        return $collection;
+    }
+
+    /**
+     * @return void
+     */
+    protected function getCurrentCategoryId()
+    {
+        $this->currentCategoryId = $this->session->getData('last_viewed_category_id');
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Magento 2 Product Menu Extension
+
+This Magento 2 module displays 3 simple products of the selected category in the sidebar. When you refresh the page, the products change.
+
+![Example](docs/details.jpg)
+
+## Requirements
+* Magento Community Edition 2.1.x-2.4.x or Magento Enterprise Edition 2.1.x-2.4.x
+
+## Installation
+* Download [ZIP Archive](https://github.com/nordraw/magento2-product-menu/archive/refs/heads/master.zip)
+* Extract files
+* In your Magento 2 root directory create folder app/code/Tretiak/ProductMenu
+* Copy files and folders from archive to that folder
+* Run commands:
+```
+php bin/magento module:enable Tretiak_ProductMenu
+php bin/magento setup:upgrade
+```
+
+## Have questions?
+Questions and wishes write to email:
+nikita.tretyak093@gmail.com
